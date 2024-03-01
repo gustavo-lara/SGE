e48f55b708c32ac23dec60736129d0c97aa616ba
Gustavo Lara <gustavolara63719@gmail.com>
2024-03-01 15:50:03 -0300

    .

--- SGE/Controllers/UsuariosController.cs
+++ SGE/Controllers/UsuariosController.cs
@@ -218,7 +218,7 @@ namespace SGE.Controllers
                 }
                 else
                 {
-                    aluno.CadAtivo = true;
+                    aluno.CadAtivo = false;
                     usuario.CadInativo = null;
                     aluno.CadInativo = null;
                 }
